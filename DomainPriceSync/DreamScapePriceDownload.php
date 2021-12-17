60afd0fee0e9d4b59f4ee4cdf21c600152e63a62
Kads <25678549+qadszilla@users.noreply.github.com>
2021-12-17 23:16:21 +1000

    Fix some formatting

--- DomainPriceSync/DreamScapePriceDownload.php
+++ DomainPriceSync/DreamScapePriceDownload.php
@@ -9,21 +9,21 @@ use Illuminate\Support\Facades\Http;
 class DreamScapePriceDownload extends Command
 {
     /**
-     * The name and signature of the console command.
+     * The name and signature of the console command
      *
      * @var string
      */
     protected $signature = 'DreamScape:PriceDownload';
 
     /**
-     * The console command description.
+     * The console command description
      *
      * @var string
      */
     protected $description = 'Download domain prices from Dreamscape';
 
     /**
-     * Create a new command instance.
+     * Create a new command instance
      *
      * @return void
      */
